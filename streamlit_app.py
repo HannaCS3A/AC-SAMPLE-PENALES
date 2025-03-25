48b4631758ba87c80fe8184a92ef1bcc914d7997
HannaCS3A <halustiano@my.cspc.edu.ph>
2025-03-25 15:43:55 +0800

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("😎 Ni Hao 😁")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's be friends! 🥰)."
 )
